5353fddd66132e0ee2c81cae3a8ef3f65844473a
Michael <michael_405@outlook.com>
2023-10-03 02:39:43 +0200

    Added functions to populate array with player markers by clicking the grid

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # tic-tac-toe
 Tic Tac Toe game
+
+I feel like i'm never going to be able to do this.
---
+++ game.js
@@ -0,0 +1,64 @@
+"use strict";
+
+// Game board
+const gameBoard = (function () {
+  const rows = 3;
+  const columns = 3;
+  const board = [];
+
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push("");
+    }
+  }
+
+  const getWholeBoard = () => board[0].concat(board[1], board[2]);
+  const boardLength = getWholeBoard().length;
+
+  return { getWholeBoard, boardLength };
+})();
+
+//Player factory
+const Player = (playerNum, marker) => {
+  return { playerNum, marker };
+};
+
+const players = (function () {
+  const playerOne = Player("1", "X");
+  const playerTwo = Player("2", "O");
+  return { playerOne, playerTwo };
+})();
+
+// Game flow
+const game = (function () {
+  const board = gameBoard.getWholeBoard();
+
+  const placeMarker = (marker, e) => {
+    const position = e;
+    board.splice(position, 1, marker);
+    console.log(board);
+  };
+  return { placeMarker, board };
+})();
+
+//Display controller
+const displayControl = (function () {
+  const generateGrid = (function (gridSize) {
+    const container = document.querySelector(".container");
+    container.style.gridTemplateColumns = `repeat(${gridSize / 3}, 1fr)`;
+    for (let i = 0; i < gridSize; i++) {
+      const cell = document.createElement("div");
+      cell.dataset.position = i;
+      cell.addEventListener("click", (e) => {
+        const position = e.target.dataset.position;
+        game.placeMarker("X", position);
+        console.log(e.target.dataset.position);
+      });
+      cell.classList.add("cell");
+      container.appendChild(cell);
+    }
+  })(gameBoard.boardLength);
+
+  return { generateGrid };
+})();
