fbec504c27ef6912bcbd68dee51864c7edb0a801
Michael <michael_405@outlook.com>
2023-10-14 18:27:40 +0200

    add reset functions

--- game.js
+++ game.js
@@ -1,22 +1,29 @@
 "use strict";
 
 const gameBoard = (function () {
-  const board = [];
+  let board = [];
   for (let i = 0; i < 9; i++) {
     board.push("");
   }
 
+  const resetBoard = () => {
+    board = [];
+    for (let i = 0; i < 9; i++) {
+      board.push("");
+    }
+  };
+
   const getBoard = () => board;
 
   const setMarker = (e) => {
-    let marker = players.getActivePlayer().marker;
-
     if (board[e.id] === "") {
+      let marker = players.getActivePlayer().marker;
       getBoard().splice(e.id, 1, marker);
+      game.setTurn();
     } else return;
   };
 
-  return { getBoard, setMarker };
+  return { getBoard, setMarker, resetBoard };
 })();
 
 const Player = (marker) => {
@@ -38,6 +45,9 @@ const players = (function () {
 })();
 
 const screenController = (function () {
+  const overlay = document.getElementById("overlay");
+  const restartBtn = document.getElementById("restart-btn");
+  const statusText = document.getElementById("status");
   const container = document.querySelector(".container");
   container.style.gridTemplateColumns = `repeat(3, 1fr)`;
   const createGrid = (function () {
@@ -59,10 +69,20 @@ const screenController = (function () {
     });
   };
 
-  return { updateGrid };
+  const setStatusText = (text) => {
+    statusText.textContent = text;
+  };
+
+  const toggleOverlay = () => {
+    overlay.classList.toggle("active");
+  };
+
+  return { updateGrid, setStatusText, toggleOverlay, restartBtn };
 })();
 
 const game = (function () {
+  let turns = 0;
+  let isWin = false;
   const checkWin = () => {
     const winCombos = [
       [0, 1, 2],
@@ -80,7 +100,9 @@ const game = (function () {
         gameBoard.getBoard()[cell].includes("X")
       );
       if (xWins) {
-        alert("X wins");
+        screenController.setStatusText("X won!");
+        screenController.toggleOverlay();
+        isWin = true;
         return;
       }
 
@@ -88,10 +110,19 @@ const game = (function () {
         gameBoard.getBoard()[cell].includes("O")
       );
       if (oWins) {
-        alert("O wins");
+        screenController.setStatusText("O won!");
+        screenController.toggleOverlay();
+        isWin = true;
         return;
       }
     });
+
+    if (turns === 9 && !isWin) {
+      screenController.setStatusText("Tie!");
+      screenController.toggleOverlay();
+      return;
+    }
+    console.log({ turns });
   };
 
   const playRound = (e) => {
@@ -99,5 +130,23 @@ const game = (function () {
     screenController.updateGrid();
     checkWin();
   };
-  return { checkWin, playRound };
+
+  const setTurn = () => {
+    turns++;
+  };
+
+  const restartGame = () => {
+    turns = 0;
+    isWin = false;
+    gameBoard.resetBoard();
+    gameBoard.getBoard();
+    screenController.updateGrid();
+  };
+
+  screenController.restartBtn.addEventListener("click", () => {
+    restartGame();
+    screenController.toggleOverlay();
+  });
+
+  return { checkWin, playRound, setTurn };
 })();
